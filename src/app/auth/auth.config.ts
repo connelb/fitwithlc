a415fab890dedd9e8c902e3ccf16b5916546742d
Brian Connell <brianjconnell@hotmail.com>
2017-12-03 23:19:58 -0600

    updated audience

--- src/app/auth/auth.config.ts
+++ src/app/auth/auth.config.ts
@@ -12,10 +12,11 @@ interface AuthConfig {
 export const AUTH_CONFIG: AuthConfig = {
   CLIENT_ID: 'seb7PkmMXqiZ3oXbjd76wJGpj1HtcSAn',
   CLIENT_DOMAIN: 'balance.auth0.com', // e.g., kmaida.auth0.com
-  AUDIENCE: 'http://brianazuretest2.azurewebsites.net/api/', // e.g., http://localhost:8083/api/
+  AUDIENCE: 'https://balance.auth0.com/api/v2/', // e.g., http://localhost:8083/api/
   REDIRECT: `${ENV.BASE_URI}/callback`,
   SCOPE: 'openid profile',
   NAMESPACE: 'http://brianazuretest2.azurewebsites.net/api/roles'
 };
+
 
 
